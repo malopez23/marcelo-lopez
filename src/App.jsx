48cdbf02a58a364f0e5e23d0557fe6a3dfeef273
malopez23 <malopez.rodriguez23@gmail.com>
2025-05-27 17:11:30 -0300

    commit inicial

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className='w-full h-screen flex items-center justify-center bg-indigo-800'>
+        <h1 className='text-center text-blue-300 text-6xl'>Testando o TailwindCSS</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
